465ee9b6de862c8cd5973270617a182d23f30daf
yinchaogao <yinchaogao@deeproute.ai>
2023-03-31 18:15:19 +0800

    format code

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget -O - https://downloads.openwrt.org/snapshots/targets/${target/-/\/}/openwrt-imagebuilder-${target}.Linux-x86_64.tar.xz | tar -xJ
+TARGET=${TARGET:-bcm27xx-bcm2711}
+wget -O - https://downloads.openwrt.org/snapshots/targets/${TARGET/-/\/}/openwrt-imagebuilder-${TARGET}.Linux-x86_64.tar.xz | tar -xJ
 cd openwrt-imagebuilder-*
 make info
 make image PACKAGES="$PACKAGES"
