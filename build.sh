48522cd948d1e18f27f73dbd35b1621d5422442c
xundaoxd <xundaoxd@outlook.com>
2023-04-01 13:34:08 +0800

    demo

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 TARGET=${TARGET:-bcm27xx-bcm2711}
-PACKAGES="luci \
-    shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-rules shadowsocks-libev-ss-tunnel luci-app-shadowsocks-libev \
-    luci-app-samba4 \
-    openconnect openssl-util openvpn-openssl luci-app-openvpn \
-    etherwake luci-app-wol"
 
-wget -q -O - https://downloads.openwrt.org/snapshots/targets/${TARGET/-/\/}/openwrt-imagebuilder-${TARGET}.Linux-x86_64.tar.xz | tar -xJ
-cd openwrt-imagebuilder-${TARGET}*
+git clone -b v21.02.3 https://git.openwrt.org/openwrt/openwrt.git openwrt-${TARGET}
 
-make info
-make image PACKAGES="$PACKAGES"
+pushd openwrt-${TARGET}
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+popd
 
