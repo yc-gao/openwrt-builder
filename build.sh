56ee3ec163e2c7530146e78264821cc81fb50d74
xundaoxd <xundaoxd@outlook.com>
2023-04-01 13:42:54 +0800

    demo

--- build.sh
+++ build.sh
@@ -2,13 +2,21 @@
 
 TARGET=${TARGET:-bcm27xx-bcm2711}
 
-[[ ! -d openwrt-${TARGET} ]] && git clone -b v21.02.3 https://git.openwrt.org/openwrt/openwrt.git openwrt-${TARGET}
+[[ ! -d openwrt-${TARGET} ]] && git clone https://git.openwrt.org/openwrt/openwrt.git openwrt-${TARGET}
 
 pushd openwrt-${TARGET}
 
-ls -a -l
+git pull
+git checkout ${VERSION}
+wget https://downloads.openwrt.org/releases/${VERSION/v/}/targets/${TARGET/\//-}/config.buildinfo -O .config
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+make -j$(nproc) kernel_menuconfig
+ 
+# Build the firmware image
+make -j$(nproc) defconfig download clean world
 
 popd
 
