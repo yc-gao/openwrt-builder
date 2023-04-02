f9b53394f11a62a479451e5ecfcc2b72a92e8640
xundaoxd <xundaoxd@outlook.com>
2023-04-02 13:08:06 +0800

    use release source

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-git clone https://github.com/coolsnowwolf/lede
+# prepare source
+wget -qO-https://github.com/coolsnowwolf/lede/archive/refs/tags/20220401.tar.gz | tar -xz
+mv lede-* lede
+
+# load config
 cp diffconfig lede/.config
 
+# compile
 pushd lede
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 make defconfig
 make download -j$(nproc)
 make V=s -j$(nproc)
-
 popd
 
