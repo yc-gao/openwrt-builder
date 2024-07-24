83175bc7910835c08f5d285e29862faf25aa3cc4
yinchaogao <yc_X@outlook.com>
2024-07-24 15:39:04 +0800

    update

--- tools/qemu.sh
+++ tools/qemu.sh
@@ -7,9 +7,9 @@ qemu-system-x86_64 \
     -cpu host,kvm=off \
     -m 1G \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
-    -drive format=raw,file=openwrt-x86-64-generic-ext4-combined-efi.img \
-    -nic user,net=192.168.10.0/24,hostfwd=:8080-:80 \
+    -drive format=raw,file=openwrt-x86-64-generic-squashfs-combined-efi.img \
     -nic user \
+    -nic user,net=192.168.1.0/24,hostfwd=tcp:127.0.0.1:9080-192.168.1.1:80 \
     -nic user \
     -nic user
 
