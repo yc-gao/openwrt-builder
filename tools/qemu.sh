d0dc84b104562d14ee1a1a9a3f1cad0fb02a95e7
ycgao <yc_x@outlook.com>
2024-10-19 00:47:23 +0800

    update

--- tools/qemu.sh
+++ tools/qemu.sh
@@ -7,7 +7,7 @@ qemu-system-x86_64 \
     -cpu host,kvm=off \
     -m 1G \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
-    -drive format=raw,file=./immortalwrt-x86-64-generic-ext4-combined-efi.img \
-    -nic user,net=192.168.1.0/24,hostfwd=tcp:127.0.0.1:9080-192.168.1.1:80 \
+    -drive format=raw,file=./openwrt/immortalwrt-x86-64-generic-ext4-combined-efi.img \
+    -nic user,net=192.168.1.0/24,hostfwd=tcp:127.0.0.1:9080-192.168.1.1:80,hostfwd=tcp:127.0.0.1:9090-192.168.1.1:9090 \
     -nic user
 
